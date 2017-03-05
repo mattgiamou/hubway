00d5f4d5e9acdabbed5e38555d9c16644d094a03
Matt Giamou <matthew.giamou@gmail.com>
2017-03-05 17:52:05 -0500

    Fixed bad end station ID with pandas read-in.

--- hubway/dataset_io.py
+++ hubway/dataset_io.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import pandas as pd
 
 data_path = '/drive1/Datasets/hubway/'
@@ -12,7 +13,20 @@ def get_stations(path=station_path):
   return df
 
 def get_trip_data(year, month, path=tripdata_path):
-  pass
+  trip_path = tripdata_path + str(year) + str(month).zfill(2) + '-hubway-tripdata.csv'
+  return pd.read_csv(trip_path)
+
+def get_all_trip_data():
+  years = [2015, 2016]
+  months = np.arange(1,13, dtype='int')
+  dfs = []
+  for year in years:
+    for month in months:
+      df = get_trip_data(year, month)
+      df = df[~df['end station id'].isin(['\\N'])]
+      df['end station id'] = df['end station id'].astype(int)
+      dfs.append(df)
+  return pd.concat(dfs, ignore_index=True)
 
 if __name__=='__main__':
 
